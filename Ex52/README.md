fbd8afd4081c0f64cc895805422e199546183476
Nick <nicolas.s.shu@gmail.com>
2018-04-27 16:13:27 -0400

    Added Ex52

---
+++ Ex52/README.md
@@ -0,0 +1,9 @@
+# Urho3D-Template
+An Urho3D template. Just download and get crackin' .
+
+Includes a basic settings loader.
+
+To use:
+  - Make sure your URHO3D_HOME environment variable is set.
+  - Copy the bin folder over from your Urho3D installation.
+  - Move Settings.json into the bin folder.
---
+++ Ex52/cmake-build-debug/CMakeFiles/SecondProject.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nickshu/GithubRepos/SecondProject/Source/Character2D.cpp" "/home/nickshu/GithubRepos/SecondProject/cmake-build-debug/CMakeFiles/SecondProject.dir/Source/Character2D.cpp.o"
+  "/home/nickshu/GithubRepos/SecondProject/Source/Mover.cpp" "/home/nickshu/GithubRepos/SecondProject/cmake-build-debug/CMakeFiles/SecondProject.dir/Source/Mover.cpp.o"
+  "/home/nickshu/GithubRepos/SecondProject/Source/Sample2D.cpp" "/home/nickshu/GithubRepos/SecondProject/cmake-build-debug/CMakeFiles/SecondProject.dir/Source/Sample2D.cpp.o"
+  "/home/nickshu/GithubRepos/SecondProject/Source/Urho2DIsometricDemo.cpp" "/home/nickshu/GithubRepos/SecondProject/cmake-build-debug/CMakeFiles/SecondProject.dir/Source/Urho2DIsometricDemo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "URHO3D_ANGELSCRIPT"
+  "URHO3D_IK"
+  "URHO3D_LOGGING"
+  "URHO3D_LUA"
+  "URHO3D_NAVIGATION"
+  "URHO3D_NETWORK"
+  "URHO3D_PHYSICS"
+  "URHO3D_PROFILING"
+  "URHO3D_STATIC_DEFINE"
+  "URHO3D_THREADING"
+  "URHO3D_URHO2D"
+  "URHO3D_WEBP"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Urho3D/include"
+  "../Urho3D/include/Urho3D/ThirdParty"
+  "../Urho3D/include/Urho3D/ThirdParty/Bullet"
+  "../Urho3D/include/Urho3D/ThirdParty/Lua"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
